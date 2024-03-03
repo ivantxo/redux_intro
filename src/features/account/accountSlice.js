ab236cca05f7e3fd64805385a1c33bc43e1c2b33
IvanR <ivan.ramirezf@gmail.com>
2024-03-04 06:12:48 +1000

    action creator with 2 parameters
    - There are some extra steps when calling an action creator with 2 or
    more parameters

--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -15,22 +15,29 @@ const accountSlice = createSlice({
       state.balance += action.payload;
     },
 
-    witdraw(state, action) {
+    withdraw(state, action) {
       state.balance -= action.payload;
     },
 
-    requestLoan(state, action) {
-      if (state.loan > 0) return;
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
 
-      state.loan = action.payload.amount;
-      state.loanPurpose = action.payload.purpose;
-      state.balance = state.balance + action.payload.amount;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
     },
 
     payLoan(state, action) {
+      state.balance -= state.loan;
       state.loan = 0;
       state.loanPurpose = "";
-      state.balance -= state.loan;
     },
   },
 });
@@ -38,72 +45,3 @@ const accountSlice = createSlice({
 export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
 
 export default accountSlice.reducer;
-
-// export default function accountReducer(state = initialStateAccount, action) {
-//   switch (action.type) {
-//     case "account/deposit":
-//       return {
-//         ...state,
-//         balance: state.balance + action.payload,
-//         isLoading: false,
-//       };
-
-//     case "account/withdraw":
-//       return { ...state, balance: state.balance - action.payload };
-
-//     case "account/requestLoan":
-//       if (state.loan > 0) return state;
-//       // LATER
-//       return {
-//         ...state,
-//         loan: action.payload.amount,
-//         loanPurpose: action.payload.purpose,
-//         balance: state.balance + action.payload.amount,
-//       };
-
-//     case "account/payLoan":
-//       return {
-//         ...state,
-//         loan: 0,
-//         loanPurpose: "",
-//         balance: state.balance - state.loan,
-//       };
-
-//     case "account/convertingCurrency":
-//       return { ...state, isLoading: true };
-
-//     default:
-//       return state;
-//   }
-// }
-
-// // action creators
-// export function deposit(amount, currency) {
-//   if (currency === "USD") return { type: "account/deposit", payload: amount };
-
-//   return async function (dispatch, getState) {
-//     dispatch({ type: "account/convertingCurrency" });
-//     const res = await fetch(
-//       `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
-//     );
-//     const data = await res.json();
-//     const converted = data.rates.USD;
-
-//     dispatch({ type: "account/deposit", payload: converted });
-//   };
-// }
-
-// export function withdraw(amount) {
-//   return { type: "account/withdraw", payload: amount };
-// }
-
-// export function requestLoan(loanAmount, loanPurpose) {
-//   return {
-//     type: "account/requestLoan",
-//     payload: { amount: loanAmount, purpose: loanPurpose },
-//   };
-// }
-
-// export function payLoan() {
-//   return { type: "account/payLoan" };
-// }
